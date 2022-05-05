924b8208a6fd6425e39b1cd99cbcddf7360890fa
Avinash Kumar <ak2449746@gmail.com>
2022-05-05 22:23:03 +0530

    update...

--- src/screens/QuizSection.js
+++ src/screens/QuizSection.js
@@ -1,8 +1,67 @@
-import React from 'react';
+import { CircularProgress } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import Question from '../components/Question';
+import '../styles/QuizSection.css';
 
-const QuizSection = () => {
+
+
+const QuizSection = ({ name, questions, score, setScore, setQuestions }) => {
+
+    const [options, setOptions] = useState()
+    const [currQues, setCurrQues] = useState(0)
+
+    useEffect(() => {
+        setOptions(
+            questions &&
+            handleShuffle([
+                questions[currQues]?.correct_answer,
+                ...questions[currQues]?.incorrect_answers,
+            ])
+        )
+    }, [currQues, questions])
+
+    console.log(questions);
+
+    const handleShuffle = (options) => {
+        return options.sort(() => Math.random() - 0.5)
+    }
+
+
+
+
     return (
-        <div>QuizSection</div>
+        <div className="quiz">
+            <span className="subtitle">Welcome, {name}</span>
+
+            {questions ? (
+                <>
+                    <div className="quizInfo">
+                        <span>{questions[currQues].category}</span>
+                        <span>
+                            {/* {questions[currQues].difficulty} */}
+                            Score : {score}
+                        </span>
+                    </div>
+                    <Question
+                        currQues={currQues}
+                        setCurrQues={setCurrQues}
+                        questions={questions}
+                        options={options}
+                        correct={questions[currQues]?.correct_answer}
+                        score={score}
+                        setScore={setScore}
+                        setQuestions={setQuestions}
+                    />
+                </>
+            ) : (
+                <CircularProgress
+                    style={{ margin: 100 }}
+                    color="inherit"
+                    size={150}
+                    thickness={1}
+                />
+            )}
+        </div>
     )
 }
 
