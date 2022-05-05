28bea726241e79846a479bd802c8ac794334f86e
Avinash Kumar <ak2449746@gmail.com>
2022-05-05 22:28:08 +0530

    update...

--- src/screens/QuizResult.js
+++ src/screens/QuizResult.js
@@ -1,8 +1,34 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import '../styles/QuizResult.css';
 
-const QuizResult = () => {
+const QuizResult = ({ name, score }) => {
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!name) {
+            navigate("/");
+        }
+    }, [name, navigate]);
+
+
+
     return (
-        <div>QuizResult</div>
+        <div className="result">
+            <span className="title">Final Score : {score}</span>
+            <Button
+                variant="contained"
+                color="secondary"
+                size="large"
+                style={{ alignSelf: "center", marginTop: 20 }}
+                href="/"
+            >
+                Go to homepage
+            </Button>
+        </div>
     )
 }
 
